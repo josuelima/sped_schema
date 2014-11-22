ec3cdab21f8ed327c5a132d27a5bd35f237c91c1
Josué Lima <josuedsi@gmail.com>
2014-11-22 11:32:52 -0300

    Update travis_script.rb description

--- travis_script.rb
+++ travis_script.rb
@@ -1,14 +1,14 @@
-# Desconsidere este arquivo. utilizado apenas pelo travis.ci para
-# testar os schemas
-
-collate = 'latin1_general_ci'
-engine  = 'MyISAM'
-charset = 'latin1'
+# Script ruby utilizado pelo travis para definir o charset e engine
+# dos schemas mysql. 
+# Utilizado pelo travis ci para definir estas
+# variaveis antes de executar os testes para validar o schemas.
+# Reaproveite esse arquivo para definir o charset, collate e engine
+# dos seus schemas caso nao queira fazer isso manualmente.
 
 Dir["./**/*.sql"].each do |arquivo|
   conteudo = IO.read(arquivo)
-               .gsub('{COLLATE}', collate)
-               .gsub('{CHARSET}', charset)
-               .gsub('{ENGINE}',  engine)
+               .gsub('{COLLATE}', 'latin1_general_ci')
+               .gsub('{CHARSET}', 'MyISAM')
+               .gsub('{ENGINE}',  'latin1')
   IO.write(arquivo, conteudo)
 end
